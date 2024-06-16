76084056c9810e1590120fb29a10d4700f9d4010
Yuriy Dobryanskiy <juriydobryansky@gmail.com>
2024-06-16 03:26:17 +0300

    ?

--- lesson-22/task-4/index.js
+++ lesson-22/task-4/index.js
@@ -1,10 +1,4 @@
-const textInput = document.querySelector('.text-input');
-const checkbox = document.querySelector('.task-status');
-
-textInput.addEventListener('input', () => {
-  console.log(`Checkbox is checked: ${checkbox.checked}`);
-});
-
-checkbox.addEventListener('change', () => {
-  console.log(`Checkbox is checked: ${checkbox.checked}`);
+const checkboxElem = document.querySelector('.task-status');
+checkboxElem.addEventListener('change', () => {
+  console.log(checkboxElem.checked);
 });
