addb0f0e85a8d0a40fde9e5f3e58fac9e52eea84
Yuriy Dobryanskiy <juriydobryansky@gmail.com>
2024-06-16 23:28:35 +0300

    task-2

---
+++ lesson-23/task-2/index.js
@@ -0,0 +1,44 @@
+const formElem = document.querySelector('.login-form');
+const emailElem = document.querySelector('#email');
+const passwordElem = document.querySelector('#password');
+const emailErrorElem = document.querySelector('.error-text_email');
+const passwordErrorElem = document.querySelector('.error-text_password');
+
+const validateEmail = () => {
+  const emailValue = emailElem.value.trim();
+  let message = '';
+  if (!emailValue) {
+    message = 'Required';
+  } else if (!emailValue.includes('@')) {
+    message = 'Should be an email';
+  }
+  emailErrorElem.textContent = message;
+  emailElem.classList.toggle('invalid', !!message);
+  return !message;
+};
+
+const validatePassword = () => {
+  const passwordValue = passwordElem.value.trim();
+  let message = '';
+  if (!passwordValue) {
+    message = 'Required';
+  }
+  passwordErrorElem.textContent = message;
+  passwordElem.classList.toggle('invalid', !!message);
+  return !message;
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const isEmailValid = validateEmail();
+  const isPasswordValid = validatePassword();
+
+  if (isEmailValid && isPasswordValid) {
+    const formData = Object.fromEntries(new FormData(formElem));
+    alert(JSON.stringify(formData));
+  }
+};
+
+emailElem.addEventListener('input', validateEmail);
+passwordElem.addEventListener('input', validatePassword);
+formElem.addEventListener('submit', handleSubmit);
