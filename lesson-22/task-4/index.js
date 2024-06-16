b36227990880a7c093366eb0b57f1501962e65d8
Yuriy Dobryanskiy <juriydobryansky@gmail.com>
2024-06-16 03:22:48 +0300

    ?

--- lesson-22/task-4/index.js
+++ lesson-22/task-4/index.js
@@ -2,5 +2,4 @@ const checkbox = document.querySelector('.task-status');
 
 checkbox.addEventListener('change', () => {
   console.log(`Checked: ${checkbox.checked}`);
-  console.log(`Value: ${checkbox.value}`);
 });
